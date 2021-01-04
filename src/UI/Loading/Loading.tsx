17506800ed9c5f4b2dfa94085709a4450b39d8a5
arianna choza <unachoza@gmail.com>
2021-01-03 19:09:39 -0800

    load spinner plus few animations

--- src/UI/Loading/Loading.tsx
+++ src/UI/Loading/Loading.tsx
@@ -2,22 +2,7 @@ import React from 'react';
 import 'UI/Loading/Loading.styles.css';
 
 const Loading = (): JSX.Element => {
-  return (
-    <div className="load-spinner">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  );
+  return <div className="load-spinner">{Array(10).fill(<div></div>)}</div>;
 };
 
 export default Loading;
